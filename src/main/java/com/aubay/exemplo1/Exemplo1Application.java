c2203952a9f96e06833f2f46930c858cd0bc9ff6
Fagner Candido <fsouzacandido@gmail.com>
2022-03-19 08:48:12 +0000

    fix class error

--- src/main/java/com/aubay/exemplo1/Exemplo1Application.java
+++ src/main/java/com/aubay/exemplo1/Exemplo1Application.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Exemplo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exemplo1Application.class, args);
+		SpringApplication.;run(Exemplo1Application.class, args);
 	}
 
 }
-;
